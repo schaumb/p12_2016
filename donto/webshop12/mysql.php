cc953a83b787990b07392c6e4d503caa9c77b1fc
Komonyi <komonyilaszlo@gmail.com>
2016-10-13 15:31:56 +0200

    Webshop12
    Felesleges dolgok

--- donto/webshop12/mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once("config.php");
-
-try {
-    $MYSQL = new PDO("mysql:=" . MYSQL_DATABASE_NAME . ";host=" . MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
